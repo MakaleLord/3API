587ec0c7e2d7ca529f54229ad003721f7b612aa1
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-07-29 13:42:59 -0500

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,29 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+
+table{
+  background-color: white;
+  border-radius: 3px;
+  font-family: 'Open Sans', sans-serif;
+  font-size:18px;
+  background-color:#34495e;
+  table-layout: fixed;
+  margin:30px auto;
+
+}
+
+td, th{
+  padding:18px;
+  border-radius: 3px;
+  background-color: #2c3e50;
+  color:white;
+  box-shadow: 2px 2px 5px #34495e;
+  text-align:center;
+}
+
+th{
+  background-color: #2980b9;
+}
+</style>
+
