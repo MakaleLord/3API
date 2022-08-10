79e0e61e215c0dd66994eb816ada9f6bb0442e4b
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-08-10 16:27:32 -0500

    Add files via upload

---
+++ wizard.py
@@ -0,0 +1,61 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+
+table{
+  background-color: white;
+  border-radius: 3px;
+  font-family: 'Open Sans', sans-serif;
+  font-size:18px;
+  background-color:#34495e;
+  table-layout: fixed;
+  margin:30px auto;
+
+}
+
+td, th{
+  padding:18px;
+  border-radius: 3px;
+  background-color: #2c3e50;
+  color:white;
+  box-shadow: 2px 2px 5px #34495e;
+  text-align:center;
+}
+
+th{
+  background-color: #2980b9;
+}
+</style>
+
+<table><tbody>
+<tr>
+<th> name </th>
+<th> wizardPoints </th>
+</tr>
+</tbody></table>
+<table><tbody>
+<tr>
+<th> name </th>
+<th> wizardPoints </th>
+</tr>
+<tr>
+<td> Yuki Jones </td>
+<td> 16070 </td>
+</tr>
+<tr>
+<td> Abdullah Shah </td>
+<td> 14972 </td>
+</tr>
+<tr>
+<td> Makale Lord </td>
+<td> 14680 </td>
+</tr>
+<tr>
+<td> Stinger Holland </td>
+<td> 13345 </td>
+</tr>
+<tr>
+<td> Yashas Mushrif </td>
+<td> 11675 </td>
+</tr>
+</tbody></table>
